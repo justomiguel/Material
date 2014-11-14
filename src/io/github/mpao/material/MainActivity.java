0cd237fc89e3281c16a32cdb9abdf43821a518d7
marco <mpao@me.com>
2014-11-14 12:35:56 +0100

    stand alone toolbar

--- src/io/github/mpao/material/MainActivity.java
+++ src/io/github/mpao/material/MainActivity.java
@@ -26,43 +26,31 @@
  *******************************************************************************/
 package io.github.mpao.material;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
+public class MainActivity extends Activity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
 
-public class MainActivity extends ActionBarActivity {
+	    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (toolbar != null) {
-        	setSupportActionBar(toolbar);
-        }        
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	    // Set an OnMenuItemClickListener to handle menu item clicks
+	    toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+	        @Override
+	        public boolean onMenuItemClick(MenuItem item) {
+	            // Handle the menu item
+	            return true;
+	        }
+	    });
+	 // Inflate a title to be displayed in the toolbar
+	    toolbar.setTitle(R.string.app_name);
+	    // Inflate a menu to be displayed in the toolbar
+	    toolbar.inflateMenu(R.menu.main);
+	}
 }
